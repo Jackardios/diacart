42a716b3e7c7d167f4056654d45ee3119df77211
Jackardios <salahutdinov.salavat@gmail.com>
2018-11-27 16:36:00 +0300

    Empty root fix

--- src/components/diacartCounterInit.js
+++ src/components/diacartCounterInit.js
@@ -1,31 +1,33 @@
 import { addDelegatedEventListener, triggerEvent } from "../utils/helpers";
 
 const DEFAULT_SELECTORS = {
-  wrapper: '.diacart-counter',
-  buttonPlus: '.diacart-counter__plus-btn',
-  buttonMinus: '.diacart-counter__minus-btn',
-  input: '.diacart-counter__input',
-}
+  wrapper: ".diacart-counter",
+  buttonPlus: ".diacart-counter__plus-btn",
+  buttonMinus: ".diacart-counter__minus-btn",
+  input: ".diacart-counter__input"
+};
 
 export default (root = document, selectors = DEFAULT_SELECTORS) => {
-  addDelegatedEventListener(root, 'click', selectors.buttonPlus, function() {
-    const wrapper = this.closest(selectors.wrapper),
-          input = wrapper.querySelector(selectors.input),
-          inputValue = parseInt(input.value),
-          newValue = inputValue > -1 ? inputValue + 1 : 1;
+  if (root) {
+    addDelegatedEventListener(root, "click", selectors.buttonPlus, function() {
+      const wrapper = this.closest(selectors.wrapper),
+        input = wrapper.querySelector(selectors.input),
+        inputValue = parseInt(input.value),
+        newValue = inputValue > -1 ? inputValue + 1 : 1;
 
-    input.value = newValue;
+      input.value = newValue;
 
-    triggerEvent(input, 'change');
-  });
+      triggerEvent(input, "change");
+    });
 
-  addDelegatedEventListener(root, 'click', selectors.buttonMinus, function() {
-    const wrapper = this.closest(selectors.wrapper),
-          input = wrapper.querySelector(selectors.input),
-          inputValue = parseInt(input.value),
-          newValue = inputValue > 1 ? inputValue - 1 : inputValue;
+    addDelegatedEventListener(root, "click", selectors.buttonMinus, function() {
+      const wrapper = this.closest(selectors.wrapper),
+        input = wrapper.querySelector(selectors.input),
+        inputValue = parseInt(input.value),
+        newValue = inputValue > 1 ? inputValue - 1 : inputValue;
 
-    input.value = newValue;
-    triggerEvent(input, 'change');
-  });
-}
+      input.value = newValue;
+      triggerEvent(input, "change");
+    });
+  }
+};
